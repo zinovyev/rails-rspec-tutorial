4acdfebbbb744b4062f6dd404a34740b5b0918b4
Ivan Zinovyev <vanyazin@gmail.com>
2018-02-19 17:00:43 +0300

    Update spec

--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -10,20 +10,39 @@ RSpec.feature "Articles", type: :feature do
       end
     end
 
-    scenario "Should be successful" do
+    scenario "should be successful" do
       fill_in "Body", with: "Test body"
       click_button "Create Article"
       expect(page).to have_content("Article was successfully created")
     end
 
-    scenario "Should fail" do
+    scenario "should fail" do
       click_button "Create Article"
       expect(page).to have_content("Body can't be blank")
     end
   end
 
   context "Update article" do
+    let(:article) { Article.create(title: "Test title", body: "Test content") }
+    before(:each) do
+      visit edit_article_path(article)
+    end
 
+    scenario "should be successful" do
+      within("form") do
+        fill_in "Body", with: "New body content"
+      end
+      click_button "Update Article"
+      expect(page).to have_content("Article was successfully updated")
+    end
+
+    scenario "should fail" do
+      within("form") do
+        fill_in "Body", with: ""
+      end
+      click_button "Update Article"
+      expect(page).to have_content("Body can't be blank")
+    end
   end
 
   context "Remove existing article" do
